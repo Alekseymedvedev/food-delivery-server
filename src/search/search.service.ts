26cc248cda5f5fa506cf98b9832477a4c763673a
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-27 17:09:37 +0300

    commit

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ProductsModel } from 'src/products/products.model';
 import { Op } from 'sequelize';
 import {BotService} from "../bot/bot.service";
+import {CategoriesModel} from "../categories/categories.model";
 
 @Injectable()
 export class SearchService {
@@ -12,7 +13,12 @@ export class SearchService {
           where: { title: { [Op.iLike]: `%${query["search"]}%` } },
         include:{all:true}
       });
-      return products;
+      const categories = await CategoriesModel.findAll({
+        where: { title: { [Op.iLike]: `%${query["search"]}%` } },
+        include:{all:true}
+      });
+      const productsInCategories = categories.map(item=>item.products)
+      return [...productsInCategories[0], ...products];
     }catch(e){
       await this.botService.errorMessage(`Произошла ошибка при поиске: ${e}`)
     }
