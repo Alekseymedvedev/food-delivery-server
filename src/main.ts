a64005fd79f5bce1ba19045152cd3550731616d9
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-04-23 19:10:39 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-declare const module: any;
-
-
 
 async function bootstrap() {
   try {
